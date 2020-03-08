4d3fcfa1b4f414b551263124887a29f15f13590c
Dangggchris <christophermtdang@gmail.com>
2020-03-08 15:44:30 -0700

    edit css

--- index.js
+++ index.js
@@ -25,11 +25,41 @@ $(document).ready(function() {
 
   // Initialize Carousel
   function slickCarousel () {
+    // $('.multiple-items').slick({
+    //   infinite: true,
+    //   slidesToShow: 6,
+    //   slidesToScroll: 1,
+    //   arrows: true
+    // });
     $('.multiple-items').slick({
       infinite: true,
+      speed: 300,
       slidesToShow: 6,
       slidesToScroll: 1,
-      arrows: true
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     });
   }
   
